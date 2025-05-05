97294e86f51ff3c960010a57217280e302e852f2
codinglov <git clone>
2025-05-04 23:53:22 -0500

    ain

--- module-10/import tkinter as tk.py
+++ module-10/import tkinter as tk.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 import tkinter.messagebox as msg
-import os
-import sqlite3
 
 class Todo(tk.Tk):
     def __init__(self, tasks=None):
@@ -21,7 +19,7 @@ class Todo(tk.Tk):
 
         self.tasks_canvas.configure(yscrollcommand=self.scrollbar.set)
 
-        self.title("To-Do App v3")
+        self.title("Makokha's-To do List")
         self.geometry("300x400")
 
         self.task_create = tk.Text(self.text_frame, height=3, bg="white", fg="black")
@@ -35,12 +33,13 @@ class Todo(tk.Tk):
         self.text_frame.pack(side=tk.BOTTOM, fill=tk.X)
         self.task_create.focus_set()
 
-        self.colour_schemes = [{"bg": "lightgrey", "fg": "black"}, {"bg": "grey", "fg": "white"}]
+        todo1 = tk.Label(self.tasks_frame, text="--- Add Items Here ---** Right click to Delete item""", bg="blue", fg="black", pady=10)
+        todo1.bind("<Button-1>", self.remove_task)
 
-        current_tasks = self.load_tasks()
-        for task in current_tasks:
-            task_text = task[0]
-            self.add_task(None, task_text, True)
+        self.tasks.append(todo1)
+
+        for task in self.tasks:
+            task.pack(side=tk.TOP, fill=tk.X)
 
         self.bind("<Return>", self.add_task)
         self.bind("<Configure>", self.on_frame_configure)
@@ -49,9 +48,19 @@ class Todo(tk.Tk):
         self.bind_all("<Button-5>", self.mouse_scroll)
         self.tasks_canvas.bind("<Configure>", self.task_width)
 
-    def add_task(self, event=None, task_text=None, from_db=False):
-        if not task_text:
-            task_text = self.task_create.get(1.0, tk.END).strip()
+        self.colour_schemes = [{"bg": "blue", "fg": "black"}, {"bg": "white", "fg": "black"}]
+
+#add-menu
+        self.menubar = tk.Menu(self)
+        self.config(menu=self.menubar)
+
+        self.file_menu = tk.Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label="File", menu=self.file_menu)
+
+        self.file_menu.add_command(label="Exit", command=self.quit)
+
+    def add_task(self, event=None):
+        task_text = self.task_create.get(1.0,tk.END).strip()
 
         if len(task_text) > 0:
             new_task = tk.Label(self.tasks_frame, text=task_text, pady=10)
@@ -63,22 +72,13 @@ class Todo(tk.Tk):
 
             self.tasks.append(new_task)
 
-            if not from_db:
-                self.save_task(task_text)
-
         self.task_create.delete(1.0, tk.END)
 
     def remove_task(self, event):
         task = event.widget
         if msg.askyesno("Really Delete?", "Delete " + task.cget("text") + "?"):
             self.tasks.remove(event.widget)
-
-            delete_task_query = "DELETE FROM tasks WHERE task=?"
-            delete_task_data = (task.cget("text"),)
-            self.runQuery(delete_task_query, delete_task_data)
-
             event.widget.destroy()
-
             self.recolour_tasks()
 
     def recolour_tasks(self):
@@ -111,45 +111,6 @@ class Todo(tk.Tk):
 
             self.tasks_canvas.yview_scroll(move, "units")
 
-    def save_task(self, task):
-        insert_task_query = "INSERT INTO tasks VALUES (?)"
-        insert_task_data = (task,)
-        self.runQuery(insert_task_query, insert_task_data)
-
-    def load_tasks(self):
-        load_tasks_query = "SELECT task FROM tasks"
-        my_tasks = self.runQuery(load_tasks_query, receive=True)
-
-        return my_tasks
-
-    @staticmethod
-    def runQuery(sql, data=None, receive=False):
-        conn = sqlite3.connect("tasks.db")
-        cursor = conn.cursor()
-        if data:
-            cursor.execute(sql, data)
-        else:
-            cursor.execute(sql)
-
-        if receive:
-            return cursor.fetchall()
-        else:
-            conn.commit()
-
-        conn.close()
-
-    @staticmethod
-    def firstTimeDB():
-        create_tables = "CREATE TABLE tasks (task TEXT)"
-        Todo.runQuery(create_tables)
-
-        default_task_query = "INSERT INTO tasks VALUES (?)"
-        default_task_data = ("--- Add Items Here ---",)
-        Todo.runQuery(default_task_query, default_task_data)
-
-
 if __name__ == "__main__":
-    if not os.path.isfile("tasks.db"):
-        Todo.firstTimeDB()
     todo = Todo()
     todo.mainloop()
